84a99fd0bd49f5d3b17c1f036acb4898944cea22
shettyabhinav <medicafastresponse@gmail.com>
2026-09-05 09:16:21 +0530

    2prgs

---
+++ comm.Abhinav.Trial/ArmStrongNo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArmStrongNo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is an Armstrong number:");
+        int num = sc.nextInt();
+        int sum=0;
+        int originalNum = num;
+        for(int i=0;i<String.valueOf(num).length();i++){
+            int r = num % 10;
+            sum += Math.pow(r, String.valueOf(num).length());
+            num /= 10;
+        }
+
+        if(sum == originalNum){
+            System.out.println(originalNum + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong number.");
+        }
+    }
+}
---
+++ comm.Abhinav.Trial/CountDigits.java
@@ -0,0 +1,13 @@
+public class CountDigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to count its digits:");  
+        int num = sc.nextInt();
+        int count =0;
+        while(num!=0){
+            num /= 10;
+            count++;
+        }
+        System.out.println("Number of digits in " + num + " is: " + count);
+    }
+}
